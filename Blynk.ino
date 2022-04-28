0167ff53b2534cd30080e7b9065b189cf07975de
Saniya Mathewkutty <98536910+SaniyaMathewkutty@users.noreply.github.com>
2022-04-28 15:00:39 +0100

    Added Blynk sketch
    .ino file

---
+++ Blynk.ino
@@ -0,0 +1,12 @@
+#include <Blynk.h>
+#define BLYNK_TEMPLATE_ID "TMPLt0UebJav"
+#define BLYNK_DEVICE_NAME "Arduino board"
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
